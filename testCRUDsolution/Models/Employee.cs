973a7d5674136f5d5f10496f86314be4e6c1dcf2
filkio <72137117+filkio@users.noreply.github.com>
2020-10-17 18:08:53 +0300

    Add style

--- testCRUDsolution/Models/Employee.cs
+++ testCRUDsolution/Models/Employee.cs
@@ -15,9 +15,7 @@ namespace testCRUDsolution.Models
         public string FullName { get; set; }
         [Required(ErrorMessage = "Введите должность")]
         public string Position { get; set; }
-        [Required(ErrorMessage = "Введите дату рождения")]
-     //   [RegularExpression("(0?[1-9]|[12][0-9]|3[01])\\.(0?[1-9]|1[012])\\.((19|20)[0-9][0-9])", ErrorMessage = "Формат даты рождения дд.мм.гггг")]
-        
+        [Required(ErrorMessage = "Введите дату рождения")]        
         public DateTime DateOfBirth { get; set; }
         [Required(ErrorMessage = "Введите текст")]
         public string Text { get; set; }
